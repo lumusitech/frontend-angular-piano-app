01a2e2bda728dd9cfde44f5de41705268d0a2610
lumusitech <lumusitech@gmail.com>
2023-11-14 00:13:35 -0300

    feat: add preloading of sound to avoid big delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,25 @@ import { PianoService } from './services/piano.service';
 })
 export class AppComponent implements OnInit {
   private readonly pianoService: PianoService = inject(PianoService);
+  private audioClips: { [key: string]: HTMLAudioElement } = {};
   piano!: Piano;
 
   ngOnInit(): void {
+    // preload sounds to avoid big delay
+    this.audioClips['c4'] = new Audio('../../assets/sonidos/c4.wav');
+    this.audioClips['d4'] = new Audio('../../assets/sonidos/d4.wav');
+    this.audioClips['d4'] = new Audio('../../assets/sonidos/d4.wav');
+    this.audioClips['e4'] = new Audio('../../assets/sonidos/e4.wav');
+    this.audioClips['f4'] = new Audio('../../assets/sonidos/f4.wav');
+    this.audioClips['g4'] = new Audio('../../assets/sonidos/g4.wav');
+    this.audioClips['a4'] = new Audio('../../assets/sonidos/a4.wav');
+    this.audioClips['b4'] = new Audio('../../assets/sonidos/b4.wav');
+
     this.piano = this.pianoService.getPiano();
   }
 
-  playSound(soundUrl: string) {
-    new Audio(soundUrl).play();
+  playSound(note: string) {
+    this.audioClips[note].currentTime = 0;
+    this.audioClips[note].play();
   }
 }
