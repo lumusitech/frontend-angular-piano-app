07cc436518d751d5b4f90660c930f3be2493142f
lumusitech <lumusitech@gmail.com>
2023-11-13 23:49:54 -0300

    feat: add basic piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Piano } from './interfaces/piano.interface';
+import { PianoService } from './services/piano.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'piano';
+export class AppComponent implements OnInit {
+  private readonly pianoService: PianoService = inject(PianoService);
+  piano!: Piano;
+
+  ngOnInit(): void {
+    this.piano = this.pianoService.getPiano();
+  }
+
+  playSound(soundUrl: string) {
+    new Audio(soundUrl).play();
+  }
 }
